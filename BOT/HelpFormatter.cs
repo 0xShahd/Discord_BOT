a06b190e95474d3939ab052cc6e7efdff11bd83d
0xShahd <shahd0x01@gmail.com>
2021-05-15 03:56:14 +0200

    Add Help Formatter

--- BOT/HelpFormatter.cs
+++ BOT/HelpFormatter.cs
@@ -2,22 +2,40 @@
 using DSharpPlus.CommandsNext.Converters;
 using DSharpPlus.CommandsNext.Entities;
 using DSharpPlus.Entities;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace BOT
 {
     class HelpFormatter : DefaultHelpFormatter
     {
+        protected DiscordEmbedBuilder embed = new DiscordEmbedBuilder();
+        protected StringBuilder strb = new StringBuilder();
         public HelpFormatter(CommandContext context) : base(context) { }
-
         public override CommandHelpMessage Build()
-        {
+        {        
             EmbedBuilder.Color = DiscordColor.HotPink;
+            EmbedBuilder.Url = "";
+            EmbedBuilder.Title = "Help مساعدة";        
             return base.Build();
+        }
+        public override BaseHelpFormatter WithCommand(Command command)
+        {
+            embed.AddField(command.Name, command.Description);            
+            strb.AppendLine($"{command.Name} - {command.Description}");
+
+            return this;
+        }
+
+        public override BaseHelpFormatter WithSubcommands(IEnumerable<Command> commands)
+        {
+            foreach (var commad in commands)
+            {
+                embed.AddField(commad.Name, commad.Description);            
+                strb.AppendLine($"{commad.Name} - {commad.Description}");
+            }
+
+            return this;
         }
     }
 }
